9cea33eba9dff78e357d1ac7ae624d5b6e268a4e
Dave Lerner <pdxgeek@gmail.com>
2022-10-10 14:49:34 -0700

    Modal close updated

--- src/gamedisplay/WinModal.tsx
+++ src/gamedisplay/WinModal.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Button, Modal } from "flowbite-react";
 import GameActions from "../gamelogic/GameActions";
 import { useModalContext } from "../context/ModalContextProvider";
+import { getRandomString } from "../util/AppUtil";
+import gameActions from "../gamelogic/GameActions";
 
 const WinModal: React.FC = () => {
     const gameActions = GameActions()
@@ -11,23 +13,49 @@ const WinModal: React.FC = () => {
         setModalState({...modalState, winModal:false});
     }
 
+    const winModalText = [
+        `Oh wow it looks like ${gameActions.currentPlayer().name} has won this one.`,
+        `${gameActions.currentPlayer().name} made this game look easy.`,
+        `If you could get a job dropping checkers into lanes... ${gameActions.currentPlayer().name} would be rich!`,
+        `${gameActions.currentPlayer().name} is really good at Connect-4 ... I guess your PC is too slow for Call of Duty?`,
+        `${gameActions.currentPlayer().name} won... but then again ${gameActions.currentPlayer().name} cheated.`
+    ]
+
+    const winButtonText = [
+        "Oh Snap!",
+        "That's UnFOURtunate",
+        "Ok",
+        "The stars aligned",
+        "Well... crap",
+        "Ok",
+        "This game is rigged!",
+        "I'm just getting started",
+        "Ok",
+        "Ok",
+        "Revenge!"
+    ]
+
     return (
-        <Modal key='WinModal' show={modalState.winModal} onClose={() => closeModal()}>
-            <Modal.Header>
-                { gameActions.currentPlayer().name + " wins!"}
-            </Modal.Header>
+        <Modal key='WinModal' show={modalState.winModal}>
             <Modal.Body>
-                <div className="space-y-6">
+
+                <div>
+                    <div className='pb-5'>
+                        <span className='text-3xl text-base text-gray-400'>{ gameActions.currentPlayer().name + " wins!"}</span>
+                    </div>
+                    <div className="space-y-6 ">
                     <span className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                        {`Oh wow it looks like ${gameActions.currentPlayer().name} has won this one.`}
+                        {getRandomString(winModalText)}
                     </span>
+                    </div>
+
+                    <div className='flex grow w-full place-content-end'>
+                        <Button onClick={() => { closeModal() }}>
+                            { getRandomString(winButtonText) }
+                        </Button>
+                    </div>
                 </div>
             </Modal.Body>
-            <Modal.Footer>
-                    <Button onClick={() => { closeModal() }}>
-                        Ok
-                    </Button>
-            </Modal.Footer>
         </Modal>
     )
 }
---
+++ src/util/AppUtil.tsx
@@ -0,0 +1,8 @@
+
+function getRandomInt(max: number) {
+    return Math.floor(Math.random() * max);
+}
+
+export const getRandomString = (strings: string[]) => {
+    return strings[getRandomInt(strings.length)]
+}
